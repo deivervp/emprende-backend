8de33bfc20c8a26e1df3b940a0e725adf7997112
Deiver Velez <soporte@mdosas.com>
2023-04-26 22:55:54 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http")
+
+function requestController() {
+    //Logica de la función
+    console.log("recibimos una nueva request")
+}
+
+//Configuracion del servidor
+const server = http.createServer(requestController)
+
+server.listen(4000)
